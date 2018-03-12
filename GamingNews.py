5291ff7c17beee8754111226d876d9c2ad3d3545
tnf2152 <35643908+tnf2152@users.noreply.github.com>
2018-03-11 21:53:46 -0400

    Update GamingNews.py

--- GamingNews.py
+++ GamingNews.py
@@ -1,1 +1,26 @@
-# PythonProject
+import.praw
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+print ('Gaming News')
+
+print ('Xbox')
+
+GET [/r/XboxOne]/new
+    clamp(n, 1, 5)
+
+print ('Playstation')
+
+GET [/r/PS4]/new
+    clamp(n, 1, 5)
+
+print ('Nintendo')
+
+GET [/r/nintendo]/new
+    clamp(n, 1, 5)
+    
+print ('Uncategorized Gaming News')
+
+GET [/r/gamingnews]/new
+    clamp(n, 1, 5)
+
